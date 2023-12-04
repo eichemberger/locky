123f16235cfaca4895eba28255c436c42acdc792
German Eichemberger <germaneichemberger@gmail.com>
2023-12-03 21:18:05 -0300

    adds 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {PasswordGeneratorComponent} from "./password-generator/password-generator.component";
 import {BcryptEncryptorComponent} from "./bcrypt-encryptor/bcrypt-encryptor.component";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     path: 'encrypt/bcrypt',
     component: BcryptEncryptorComponent,
     title: 'Bcrypt Encryptor',
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+    title: 'Not found',
   }
 ];
