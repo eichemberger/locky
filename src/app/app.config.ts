dff7b57332795575c7ff6deb668707e580080fc2
German Eichemberger <germaneichemberger@gmail.com>
2024-02-25 20:16:10 +0800

    testing security config in gh

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+const PASSWORD = 'a-secret-password-132213';
+
+export function login(username: string, password: string) {
+  if (username === 'admin' && password === PASSWORD) {
+    return true;
+  }
+  return false;
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
