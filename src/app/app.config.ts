c10ec1c15b53c620d9fbd0483ebfee27f2e1c3c1
German Eichemberger <germaneichemberger@gmail.com>
2024-02-25 20:20:53 +0800

    pushes errors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,14 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 const PASSWORD = 'a-secret-password-132213';
-
+sdfdsagdfsadfdsa
 export function login(username: string, password: string) {
-  if (username === 'admin' && password === PASSWORD) {
+  if (username ==asdasd 'admin' && password === PASSWORD) {
     return true;
   }
   return false;
 }
 
-export const appConfig: ApplicationConfig = {
+export const asdasppConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
+dasdasda
