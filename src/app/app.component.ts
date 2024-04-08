11d47159888cd0aed372e2f98427a38635d17b8e
German Eichemberger <germaneichemberger@gmail.com>
2024-04-08 15:01:21 -0300

    removes rum dep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {NavComponent} from "./nav/nav.component";
 import {PasswordGeneratorComponent} from "./password-generator/password-generator.component";
 import {ToastComponent} from "./toast/toast.component";
 import {FooterComponent} from "./footer/footer.component";
-import { AwsRum, AwsRumConfig } from 'aws-rum-web';
 
 @Component({
   selector: 'app-root',
@@ -14,34 +13,6 @@ import { AwsRum, AwsRumConfig } from 'aws-rum-web';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'locky';
-
-  ngOnInit(): void {
-    try {
-      const config: AwsRumConfig = {
-        sessionSampleRate: 1,
-        guestRoleArn: "arn:aws:iam::905418360304:role/RUM-Monitor-us-east-1-905418360304-3062236952171-Unauth",
-        identityPoolId: "us-east-1:c20c3549-ba0f-47f0-b302-2c376af2f391",
-        endpoint: "https://dataplane.rum.us-east-1.amazonaws.com",
-        telemetries: ["performance","errors","http"],
-        allowCookies: false,
-        enableXRay: false
-      };
-
-      const APPLICATION_ID: string = 'c51f8fdb-a842-4f80-b772-4f565d62567b';
-      const APPLICATION_VERSION: string = '1.0.0';
-      const APPLICATION_REGION: string = 'us-east-1';
-
-      const awsRum: AwsRum = new AwsRum(
-        APPLICATION_ID,
-        APPLICATION_VERSION,
-        APPLICATION_REGION,
-        config
-      );
-    } catch (error) {
-      console.warn('Error initializing CloudWatch RUM web client', error);
-      // Ignore errors thrown during CloudWatch RUM web client initialization
-    }
-  }
 }
